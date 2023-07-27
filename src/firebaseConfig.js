0c551f87d59d5d67face8ee085e6f633c451bd77
monicampos <monica.alejandra.campos@gmail.com>
2023-07-27 19:31:42 -0300

    4ta versión Proyecto final: protección de firebase en .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyALX7g5QjOauK4UDU3fvYDWPx0mftCISWU",
-    authDomain: "dermocosmetica-react.firebaseapp.com",
-    projectId: "dermocosmetica-react",
-    storageBucket: "dermocosmetica-react.appspot.com",
-    messagingSenderId: "683945049306",
-    appId: "1:683945049306:web:62bb554ad6b6e453d2f9e3",
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
